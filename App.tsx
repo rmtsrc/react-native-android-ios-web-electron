bf4edb915400555756bd90b46a4c6d04f4946918
rmt/src <144435+rmtsrc@users.noreply.github.com>
2020-04-08 21:31:45 +0100

    Fixed dev electron command + platform detection

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 export default function App() {
+  const platform = Platform.OS === 'web' && navigator.userAgent.includes('Electron') ? 'electron' : Platform.OS;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Current platform: {platform}</Text>
     </View>
   );
 }
